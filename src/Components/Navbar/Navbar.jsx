6bc231d81099ac787572e57c69284637e9508c8b
Gastón Acosta <gaston.1693@gmail.com>
2023-04-02 21:40:52 +0200

    cambios para deploy

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,4 +1,4 @@
-import { Grid, Button } from "@mui/material";
+import { Grid } from "@mui/material";
 import { Box } from "@mui/system";
 import CartWidget from "../CartWidget/CartWidget";
 import { Link } from "react-router-dom";
